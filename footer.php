85eb7d5bfb0205e1ffa191f3ad9658ab166a04dd
IvanZakon <64014572+IvanZakon@users.noreply.github.com>
2021-02-14 18:40:27 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,94 @@
+<footer>
+ <div class="container-fluid foot-back-body">
+  <div class="container foot-body">
+    <div class="top-foot">
+      <div class="foot-info-null">
+        <ul class="foot-ul">
+          <li class="first-li-foot foot-logo"><span style="font-size: 30px; color: white;">VN</span><span style="color: #7AB1FF; font-size: 30px; ">TECHNO</span></li>
+          <li class="foot-li"><a href="" class="foot-li-a">О компании</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Реквизиты</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Оплата</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Цены</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Наши работы</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Новости</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Контакты</a></li>
+        </ul>
+      </div>
+      <div class="foot-info-one">
+        <ul class="foot-ul">
+          <li class="first-li-foot">ИЗГОТОВЛЕНИЕ МЕТАЛЛОКОНСТРУКЦИЙ</li>
+          <li class="foot-li"><a href="" class="foot-li-a">Производство изделий из металла</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Сварочные работы</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Порошковая покраска</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Плазменная резка</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Слесарные работы</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Слесарные работы</a></li>
+        </ul>
+      </div>
+      <div class="foot-info-two">
+        <ul class="foot-ul">
+          <li class="first-li-foot">ОБСЛУЖИВАНИЕ ИНЖЕНЕРНЫХ СИСТЕМ</li>
+          <li class="foot-li"><a href="" class="foot-li-a">Проверка безопастности электрических систем</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Проверка тепловых узлов и пунктов</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Чистка, ревизия и ремонт кондиционирования</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Проверка и ремонт систем водоснабжения</a></li>
+        </ul>
+      </div>
+      <div class="foot-info-three">
+        <ul class="foot-ul">
+          <li class="first-li-foot">РЕМОНТ И ОБСЛУЖИВАНИЕ ВОРОТ</li>
+          <li class="foot-li"><a href="" class="foot-li-a">Въездные ворота</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Гаражные ворота</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Промышленные ворота</a></li>
+        </ul>
+      </div>
+      <div class="foot-info-four">
+      <ul class="foot-ul foot-ul-custom-mobile">
+          <li class="first-li-foot">ПРОМЫШЛЕННЫЕ НАЛИВНЫЕ ПОЛЫ</li>
+          <li class="foot-li"><a href="" class="foot-li-a">Полимерное покрытие пола</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Пол с бетонным покрытием</a></li>
+          <li class="foot-li"><a href="" class="foot-li-a">Напольная разметка</a></li>
+        </ul>
+        <div class="footer-contact-place">
+          <div class="mail-vunosk">
+            <span class="mail-vun-one"><img src="img/svg/email.svg" class="svg-cont-mail">vntechno@mail.ru</span>
+            <span class="number-vub-one"><img src="img/svg/phone-call.svg" class="svg-cont">+7 (967)-431-08-04</span>
+            <span class="number-vub-two"><img src="img/svg/phone-call.svg" class="svg-cont">+7 (967)-431-08-04</span>
+          </div>
+          <div class="messanger-vunosk">
+            <img src="img/svg/whatsapp.svg" class="about-icons-messanger-wts">
+            <img src="img/svg/telegram.svg" class="about-icons-messanger-tg">   
+            <img src="img/svg/viber.svg" class="about-icons-messanger-viber">   
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-foot">
+      <div class="foot-logslog"><span style="font-size: 18px;">VN</span><span style="color: #7AB1FF; font-size: 18px; ">TECHNO</span><br><br>- Комплексные работы</div>
+      <div class="foot-about">
+        <div class="foot-about-first"><span>Принимаем к оплате:</span></div>
+        <div class="foot-about-twoo">
+        <img src="img/svg/mir.svg" class="about-icons-last">
+        <img src="img/svg/che.svg" class="about-icons-middle">
+        <img src="img/svg/qiwi.svg" class="about-icons-middle">
+        <img src="img/svg/visa.svg" class="about-icons-middle">
+        <img src="img/svg/mastercart.svg" class="about-icons-middle">
+        <img src="img/svg/eweodin.svg" class="about-icons-last">
+        </div>
+      </div>
+      <div class="foot-answer">
+        <div class="foot-answer-first"><span>Нужна помощь?</span></div>
+        <div class="foot-answer-two"><a href="" class="my-buttcall-two">Задать вопрос</a></div>
+      </div>
+    </div>
+    <div class="foot-vunosk">
+      <div class="copiright-vunosk">
+        © 2020, ООО "VnTechno"
+      </div>
+      <div class="politic-vunosk">
+        <u><a href="" class="politic">Политика конфидициальности</a></u>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
